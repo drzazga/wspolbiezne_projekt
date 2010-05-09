0bd410badaa1150a5d8eafcc40f0780089e32981
drzazga <drzazga@drzazga-laptop.(none)>
2010-05-09 20:40:44 +0200

    pobieranie poczatkowych wspolrzednych i idZawodnika z serwera

--- klient.c
+++ klient.c
@@ -2,6 +2,10 @@
 #include <X11/X.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <signal.h>
 
 Display *mydisplay;
 Window mywindow;
@@ -20,12 +24,35 @@ unsigned long i;
 int wspX, wspY;
 int oldX, oldY;
 int roznicaX, roznicaY;
+int idZawodnika;
 
 void rysujKolo();
 int sprawdzPolozenieKursora(int x, int y);
+void ustawWspolrzedne(char *wspolrzedne);
+void ustawDanePoczatkowe(char *dane);
+char *substring(int start, int stop, char *src, char *dst);
 
-int main()
+int main(int argc, char **argv)
 {
+  int sd,clen;
+  char *data = (char*) calloc(10, sizeof(char));
+  struct sockaddr_in sad, cad;
+
+  if(argc != 2)
+  {
+    printf("Niepoprawne wywolanie programu.\n");
+    printf("./klient ADRES_IP_SERWERA\n");
+    return -1;
+  }
+
+  sd=socket(AF_INET,SOCK_DGRAM,0);
+  //bzero(&sad, sizeof(sad));
+  sad.sin_family=AF_INET;
+  sad.sin_addr.s_addr=inet_addr(argv[1]);
+  sad.sin_port=htons((ushort) 5000);
+  bind(sd, (struct sockaddr *) &sad,sizeof(sad));
+  clen = sizeof(cad);
+
   mydisplay = XOpenDisplay("");
   myscreen = DefaultScreen(mydisplay);
   myvisual = DefaultVisual(mydisplay,myscreen);
@@ -45,10 +72,16 @@ int main()
   mycolormap = DefaultColormap(mydisplay,myscreen);
   XAllocNamedColor(mydisplay,mycolormap,"green",&mycolor,&dummy);
 
-  wspX = 200-(15/2);
-  wspY = 200-(15/2);
   oldX = -1;
   oldY = -1;
+
+  sprintf(data, "init");
+  sendto(sd, data, sizeof(data), 0, (struct sockaddr *) &sad, clen);
+  recvfrom(sd, data, 10, 0, (struct sockaddr *) &cad, &clen);
+
+  ustawDanePoczatkowe(data);
+
+  printf("id: %d, x: %d, y: %d\n", idZawodnika, wspX, wspY);
 
   while (1)
   {
@@ -99,7 +132,7 @@ int main()
       break;
 
       case Expose:
-        rysujKolo();      
+        rysujKolo();
         break;
 
       case KeyPress:
@@ -121,5 +154,38 @@ int sprawdzPolozenieKursora(int x, int y)
   if(abs(wspX-x) <= 50 && abs(wspY-y) <= 50)
     return 1;
   else
-    return 0;   
+    return 0;
 }
+
+void ustawWspolrzedne(char *wspolrzedne)
+{
+  char x[3];
+  char y[3];
+
+  substring(0, 3, wspolrzedne, x);
+  substring(3, 7, wspolrzedne, y);
+
+  wspX = atoi(x);
+  wspY = atoi(y);
+}
+
+void ustawDanePoczatkowe(char *dane)
+{
+  printf("dane: %s", dane);
+  char id[3];
+  char wspolrzedneXY[6];
+
+  substring(0, 3, dane, id);
+  substring(3, 10, dane, wspolrzedneXY);
+
+  idZawodnika = atoi(id);
+  ustawWspolrzedne(wspolrzedneXY);
+}
+
+char *substring(int start, int stop, char *src, char *dst)
+{
+  int count = stop - start;
+  sprintf(dst, "%.*s", count, src + start);
+  return dst;
+}
+
--- server.c
+++ server.c
@@ -1,38 +1,60 @@
 #define _REENTRANT
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <signal.h>
 
+typedef struct
+{
+  int id;
+  struct sockaddr adres;
+} zawodnik;
+
 void *wyslijPozycjeKola();
+void wyslijDanePoczatkowe(struct sockaddr *adres);
+char *zwrocWspolrzedne();
+int wspX, wspY;
+int sd, clen;
+char *zera[3] = { "", "0", "00" };
+int idZawodnika;
+//zawodnik tablicaZawodnikow[999];
 
 int main()
 {
-  int sd,buff,clen;
-  char *data = (char*) calloc(1, sizeof(char)); 
-  pthread_t tid;  
+  char *data = (char*) calloc(1, sizeof(char));
+  pthread_t tid;
   struct sockaddr_in sad,cad;
 
   signal(SIGHUP,SIG_IGN);
   sd=socket(AF_INET,SOCK_DGRAM,0);
-  bzero((char *) &sad, sizeof(sad));
+  //bzero((char *) &sad, sizeof(sad));
   sad.sin_family=AF_INET;
   sad.sin_addr.s_addr=htonl(INADDR_ANY);
   sad.sin_port=htons((ushort) 5000);
   bind(sd,(struct sockaddr *) &sad,sizeof(sad));
   clen=sizeof(cad);
 
+  idZawodnika = 0;
+  wspX = 100-(15/2);
+  wspY = 200-(15/2);
+
   //pthread_create(&tid,NULL,wyslijPozycjeKola,NULL);
 
   while (1)
   {
-   recvfrom(sd,(char *) &buff,sizeof(int),0,(struct sockaddr *) &cad,&clen);
-   buff=ntohl(buff);
+    recvfrom(sd, data,sizeof(data),0,(struct sockaddr *) &cad,&clen);
 
-   buff=htonl(buff);
-   sendto(sd,(char *) &buff,sizeof(int),0,(struct sockaddr *) &cad,clen);
+    if(strcmp(data, "init") == 0)
+    {
+      wyslijDanePoczatkowe((struct sockaddr *) &cad);
+    }
+    else
+    {
+      sendto(sd, data,sizeof(data),0,(struct sockaddr *) &cad,clen);
+    }
   }
 
   return 0;
@@ -42,3 +64,47 @@ void *wyslijPozycjeKola()
 {
   return;
 }
+
+void wyslijDanePoczatkowe(struct sockaddr *cad)
+{
+  char *danePoczatkowe = (char*) calloc(1, sizeof(char));
+
+  if(idZawodnika < 10)
+    sprintf(danePoczatkowe, "%s%d%s", zera[2], idZawodnika, zwrocWspolrzedne());
+  else if(idZawodnika < 100 && idZawodnika > 9)
+    sprintf(danePoczatkowe, "%s%d%s", zera[1], idZawodnika, zwrocWspolrzedne());
+  else
+    sprintf(danePoczatkowe, "%s%d%s", zera[0], idZawodnika, zwrocWspolrzedne());
+
+  printf("dane poczatkowe: %s\n", danePoczatkowe);
+
+  sendto(sd, danePoczatkowe, 10, 0, cad, clen);
+
+  idZawodnika++;
+}
+
+char *zwrocWspolrzedne()
+{
+  char *wspolrzedne = (char*) calloc(1, sizeof(char));
+  char *wspolrzednaX = (char*) calloc(1, sizeof(char));
+  char *wspolrzednaY = (char*) calloc(1, sizeof(char));
+
+  if(wspX < 10)
+    sprintf(wspolrzednaX, "%s%d", zera[2], wspX);
+  else if(wspX < 100 && wspX > 9)
+    sprintf(wspolrzednaX, "%s%d", zera[1], wspX);
+  else
+    sprintf(wspolrzednaX, "%s%d", zera[0], wspX);
+
+  if(wspY < 10)
+    sprintf(wspolrzednaY, "%s%d", zera[2], wspY);
+  else if(wspY < 100 && wspY > 9)
+    sprintf(wspolrzednaY, "%s%d", zera[1], wspY);
+  else
+    sprintf(wspolrzednaY, "%s%d", zera[0], wspY);
+
+  sprintf(wspolrzedne, "%s%s", wspolrzednaX, wspolrzednaY);
+
+  return wspolrzedne;
+}
+
